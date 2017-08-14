e37d564a25964d99d042f1fab797525bb7ce5f93
Marty Powell <jpowel25@gmail.com>
2017-08-14 09:57:27 -0400

    setting a baseline

--- bc-lumberjack.js
+++ bc-lumberjack.js
@@ -1,41 +1,111 @@
 var Lumberjack = (function () {
-    function getBrowserInfo() {
-        return {
-            "origin": window.location.origin,
-            "pathname": window.location.pathname,
-            "params": window.location.search,
-            "hash": window.location.search,
-            "language": window.navigator.language,
-            "platform": window.navigator.platform,
-            "userAgenct": window.navigator.userAgent,
+    function lumberjack() {
+        var levels = ["debug", "log", "info", "notify", "warn", "error", "fatal"];
+
+        function getBrowserInfo() {
+            return {
+                "origin": window.location.origin,
+                "pathname": window.location.pathname,
+                "params": window.location.search,
+                "hash": window.location.search,
+                "language": window.navigator.language,
+                "platform": window.navigator.platform,
+                "userAgenct": window.navigator.userAgent,
+            };
+        }
+
+        function logToConsole(level, args) {
+            if (console[level] && typeof console[level] === 'function') {
+                console[level](args, getBrowserInfo());
+            }
+            else {
+                var logType = 'log';
+                var logMessage = "generic log: ";
+
+                if (level === 'fatal') {
+                    logType = 'error';
+                    logMessage = 'fatal error: ';
+                }
+                else if (level === 'notify') {
+                    logType = 'info';
+                    logMessage = 'notification: ';
+                }
+
+                console[logType](logMessage, args, getBrowserInfo());
+            }
+        }
+
+        function logSuccessResponse(response) {
+            if (isDebugModeOn) {
+                console.log('Successful logging', response);
+            }
+        }
+
+        function logErrorResponse(xhrObj) {
+            if (isDebugModeOn) {
+                console.log('Something went wrong when logging', xhrObj);
+            }
+        }
+
+        function logToApi(level, args, browserData) {
+            var data = JSON.stringify({
+                type: level,
+                message: args[0],
+                otherData: args.shift(),
+                browserData: browserData,
+            });
+
+            postAjax(this.apiUrl, data, logSuccessResponse, logErrorResponse);
+        }
+
+        function log(level, args) {
+            if (isDebugModeOn) {
+                logToConsole(level, args);
+            }
+        }
+
+        function postAjax(url, data, success, error) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    var json = JSON.parse(xhr.responseText);
+                    success(json);
+                }
+                else {
+                    error(xhr);
+                }
+            };
+        }
+
+        return function init(apiUrl, isDebugModeOn) {
+            this.apiUrl = apiUrl;
+            this.isDebugModeOn = !!isDebugModeOn;
+
+            this.debug = function (args) {
+                log("debug", args);
+            };
+            this.log = function (args) {
+                log("log", args);
+            };
+            this.info = function (args) {
+                log("info", args);
+            };
+            this.warn = function (args) {
+                log("warn", args);
+            };
+            this.notify = function (args) {
+                log("notify", args);
+            };
+            this.error = function (args) {
+                log("error", args);
+            };
+            this.fatal = function (args) {
+                log("fatal", args);
+            };
         };
     }
 
-    function info(args) {
-        console.info(args, getBrowserInfo());
-    }
-
-    function error(args) {
-        console.error(args, getBrowserInfo());
-    }
-
-    function warn(args) {
-        console.warn(args, getBrowserInfo());
-    }
-
-    function log(args) {
-        console.log(args, getBrowserInfo());
-    }
-
-    function logToApi() {
-
-    }
-
-    return {
-        init: init,
-        info: info,
-        error: error,
-        warn: warn,
-        log: log
-    };
+    return lumberjack;
 })();
